9b1f5610e639ddda6f36a16d2db621c0e263d1d8
AjwadG <ajwadelgandouz@yahoo.com>
2024-01-21 12:42:55 -0800

    4. Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,1 +1,9 @@
-# editing servers ssh config fie to notblock pass auth
+# adding PasswordAuthentication no to servers config 
+exec { 'Turn off passwd auth':
+  command => '/usr/bin/echo "PasswordAuthentication no" >> /etc/ssh/sshd_config'
+}
+
+# adding the public key to servers auth keys
+exec { 'Declare identity file':
+  command => '/usr/bin/cat ~/.ssh/school >> /root/.ssh/authorized_keys'
+}

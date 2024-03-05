b39958f7bf6931b4dcb19f805ccd35cc76f64260
AjwadG <ajwadelgandouz@yahoo.com>
2024-03-06 00:55:26 +0200

    1. Top Ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+    1-top_ten module
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+        take one arg the subreddit name
+        sends a get req to an api
+        to get the top 10
+        returns value
+    """
+    url = "https://www.reddit.com/r/{}/top.json".format(subreddit)
+    headers = {"User-Agent": "ubuntu:Python (by/AjwadG)"}
+
+    data = requests.get(url, headers=headers, allow_redirects=False)
+
+    if data.status_code != 200:
+        print(None)
+        return
+    value = data.json().get("data")
+    if value is None:
+        print(None)
+        return
+    for post in value.get("children")[0:10]:
+        print(post.get("data").get("title"))
